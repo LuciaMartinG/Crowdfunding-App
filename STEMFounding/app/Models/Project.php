40b6425ad289e0604b2973ca7060dd134872598e
LuciaMG <luciamartinguijarro101.alu@stemgranada.com>
2025-01-10 11:22:49 +0100

    Creando migraciones y middleware.

---
+++ STEMFounding/app/Models/Project.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProjectFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'image_url',
+        'video_url',
+        'min_investment',
+        'max_investment',
+        'limit_date',
+        'state',
+    ];
+}
