79a44f40af0f969d2ae71929f850c201fa5401c5
LuciaMG <luciamartinguijarro101.alu@stemgranada.com>
2025-01-21 13:29:32 +0100

    Instalacion de axios.

--- STEMFounding_React/App.js
+++ STEMFounding_React/App.js
@@ -1,11 +1,32 @@
+import { useEffect, useState } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { getPosts } from './src/services/api';
 
 export default function App() {
+  const [projectList, setProjectList] = useState([]);
+
+  useEffect(() => {
+      getPosts().then((response) =>
+          setProjectList(response.data)
+      ).catch((error) =>
+          console.error(error)
+      );
+
+      console.log(projectList);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+
+      {projectList.map((project) => (
+
+        <Text>{project.title}</Text>
+
+      ))}
+    <StatusBar style="auto" />
     </View>
   );
 }
---
+++ STEMFounding_React/src/components/MyComponent.js
@@ -0,0 +1,3 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import api from './api';  // Importas el archivo de configuración de Axios
