32df11602352f6c95e395e197c86ad3d2a461a1f
LuciaMG <luciamartinguijarro101.alu@stemgranada.com>
2025-02-04 11:23:34 +0100

    probando withdrawFunds postman

--- STEMFounding/routes/api.php
+++ STEMFounding/routes/api.php
@@ -78,3 +78,5 @@ Route::delete('/update/{id}', function ($id) {
 Route::put('/update/{id}', [ProjectController::class, 'editUpdatePostman']); // RUTA PARA EDITAR UNA ACTUALIZACIÓN
 
 Route::post('/process-refunds', [InvestmentController::class, 'processRefunds']); //Ruta para procesar reembolsos
+
+Route::middleware('auth:sanctum')->post('/withdraw/{projectId}', [ProjectController::class, 'withdrawFundsPostman']);
