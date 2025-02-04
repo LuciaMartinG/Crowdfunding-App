e5f4a5f1277e08d74fa2ca0a94215c3dd7bca97c
LoretoLopez <145747001+LoretoLopez@users.noreply.github.com>
2025-02-04 12:02:23 +0100

    Corrigiendo seeder para que se creen proyectos activos e inactivos

--- STEMFounding/app/Providers/AppServiceProvider.php
+++ STEMFounding/app/Providers/AppServiceProvider.php
@@ -21,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->app['router']->aliasMiddleware('role', \App\Http\Middleware\RoleMiddleware::class);
-        (new InvestmentController)->processRefunds();
+        // (new InvestmentController)->processRefunds();
     }
 }
--- STEMFounding/database/factories/ProjectFactory.php
+++ STEMFounding/database/factories/ProjectFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\User;
+use App\Models\Project;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
@@ -17,17 +18,25 @@ class ProjectFactory extends Factory
      */
     public function definition(): array
     {
+        do {
+            $user = User::inRandomOrder()->first();
+            $activeProjects = Project::where('user_id', $user->id)
+                ->where('state', 'active')
+                ->count();
+        } while ($activeProjects >= 2); // Asegura que no tenga más de 2 activos
+    
         return [
             'title' => fake()->sentence(5),
-            'description'=> fake()->sentence(10),
-            'image_url'=> 'https://itequia.com/wp-content/uploads/2023/09/magnitud.jpg', 
-            'video_url'=> fake()->sentence(5),
-            'min_investment' =>fake()->randomFloat(2, 100, 100000),
-            'max_investment'=> fake()->randomFloat(2, 10000, 500000),
+            'description' => fake()->sentence(10),
+            'image_url' => 'https://itequia.com/wp-content/uploads/2023/09/magnitud.jpg',
+            'video_url' => fake()->sentence(5),
+            'min_investment' => fake()->randomFloat(2, 100, 100000),
+            'max_investment' => fake()->randomFloat(2, 10000, 500000),
             'limit_date' => fake()->dateTimeBetween('now', '+1 year'),
-            'state' => 'pending',
+            'state' => fake()->randomElement(['active', 'inactive', 'pending']),
             'current_investment' => 0,
-            'user_id' => User::inRandomOrder()->first(),
+            'user_id' => $user->id,
         ];
     }
+    
 }
